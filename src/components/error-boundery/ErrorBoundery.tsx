736851236b1f3506c4faa9043f4c07cb0fb8ba99
Kirill <kiw197204@gmail.com>
2024-07-07 19:21:40 +0400

    feat: error boundery

---
+++ src/components/error-boundery/ErrorBoundery.tsx
@@ -0,0 +1,27 @@
+import { Component, ErrorInfo, ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+};
+
+export class ErrorBoundery extends Component<Props, { error: boolean }> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      error: false,
+    };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    this.setState({
+      error: true,
+    });
+    console.log(error, errorInfo);
+  }
+  render() {
+    if (this.state.error) {
+      return <h2>Something went wrong.</h2>;
+    }
+    return this.props.children;
+  }
+}
