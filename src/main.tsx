3af49cb2c3a2477cad55e3b9066251ec8a935824
Kirill <kiw197204@gmail.com>
2024-07-14 08:29:05 +0400

    feat: Error page and router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/app/App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import { ErrorMessage } from "./components/error/error";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorMessage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
